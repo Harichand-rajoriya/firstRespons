3efa53786b15512498eddd17ec283c2b8d5746b0
Harichand-rajoriya <78351434+Harichand-rajoriya@users.noreply.github.com>
2021-02-10 10:27:45 +0530

    changes

--- repo.js
+++ repo.js
@@ -6,7 +6,7 @@ btnGen.addEventListener('click', () => {
     let{name, email, phone} = info;
     //console.log(phone);
     //alert(name);
-    document.querySelector('name').value = name;
-    document.querySelector('email').Value = email;
-    document.querySelector('phone').Value = phone;
+    document.querySelector('#name').value = name;
+    document.querySelector('#email').Value = email;
+    document.querySelector('#phone').Value = phone;
 });
